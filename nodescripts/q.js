0217d9f6849ba7ec5f76699b30f85a747fb779fc
yassh <yassh32@outlook.jp>
2016-04-01 14:42:17 +0900

    update my-q

--- nodescripts/q.js
+++ nodescripts/q.js
@@ -4,13 +4,11 @@
 
 const argv = require('yargs')
   .alias('s', 'service').describe('s', 'サービスを指定する')
+  .alias('l', 'lang').describe('l', '対象言語を指定する')
   .boolean('q').alias('q', 'quote').describe('q', '引用符で囲む')
   .boolean('w').alias('w', 'week').describe('w', '期間指定: 1週間以内')
   .boolean('m').alias('m', 'month').describe('m', '期間指定: 1か月以内')
   .boolean('y').alias('y', 'year').describe('y', '期間指定: 1年以内')
-  .boolean('j').alias('j', 'ja').describe('j', '日本語のページを検索')
-  .boolean('e').alias('e', 'en').describe('e', '英語のページを検索')
-  .boolean('r').alias('r', 'ru').describe('r', 'ロシア語のページを検索')
   .help('h').alias('h', 'help')
   .argv;
 const param = require('jquery-param');
@@ -27,9 +25,7 @@ switch (argv.service) {
 
     data.q = argv.quote ? `"${query}"` : query;
 
-    if (argv.ja) data.lang = 'ja';
-    if (argv.en) data.lang = 'en';
-    if (argv.ru) data.lang = 'ru';
+    if (argv.lang) data.lang = argv.lang;
   break;
 
   case 'l':
@@ -46,13 +42,11 @@ switch (argv.service) {
 
     data.q = argv.quote ? `"${query}"` : query;
 
+    if (argv.lang) data.lr = `lang_${argv.lang}`;
+
     if (argv.week) data.tbs = 'qdr:w';
     if (argv.month) data.tbs = 'qdr:m';
     if (argv.year) data.tbs = 'qdr:y';
-
-    if (argv.ja) data.lr = 'lang_ja';
-    if (argv.en) data.lr = 'lang_en';
-    if (argv.ru) data.lr = 'lang_ru';
   break;
 }
 
