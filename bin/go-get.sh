072519f32c762a26e59a4ec0c11091ef6102081b
yassh <yassh32@outlook.jp>
2016-04-06 20:19:19 +0900

    add go-get.sh

---
+++ bin/go-get.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "go getを開始します。"
+
+go get \
+github.com/motemen/ghq \
+;
+
+echo "go getを終了します。"
--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ $__DIR__/dotfiles/setup.sh
 
 $__DIR__/bin/brew-install.sh
 $__DIR__/bin/gem-install.sh
+$__DIR__/bin/go-get.sh
 $__DIR__/bin/npm-install.sh
 
 cd $__DIR__/nodescripts && npm link
