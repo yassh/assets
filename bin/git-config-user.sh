d4e216cc750746afb04ff35dcf3fa15b24c6b65c
yassh <yassh32@outlook.jp>
2016-04-19 22:41:20 +0900

    update git-config-user.sh

--- bin/git-config-user.sh
+++ bin/git-config-user.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 git config user.name "yassh" && git config user.email "yassh32@outlook.jp"
+git submodule foreach 'git config user.name "yassh" && git config user.email "yassh32@outlook.jp"'
