8a32ebd774036ce3620d9abb0c82fcc2f8e6c010
yassh <yassh32@outlook.jp>
2017-03-10 01:35:09 +0900

    update setup.sh (+anyenv)

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ __DIR__=$(cd $(dirname $0) && pwd)
 
 $__DIR__/dotfiles/setup.sh
 
+if [ ! -e ~/.anyenv ]; then
+  git clone https://github.com/riywo/anyenv ~/.anyenv
+fi
+
 $__DIR__/bin/brew-install.sh
 $__DIR__/bin/gem-install.sh
 $__DIR__/bin/go-get.sh
