b2542444df2e807d332f71a32da067d3341cfb3f
yassh <yassh32@outlook.jp>
2017-04-20 17:57:50 +0900

    update .emacs.d/init.el

---
+++ dotfiles/.emacs.d/init.el
@@ -0,0 +1,9 @@
+;; 行番号を表示する
+(require 'linum)
+(global-linum-mode 1)
+
+;; 空白文字を可視化する
+;; https://www.emacswiki.org/emacs/WhiteSpace
+(require 'whitespace)
+(setq whitespace-style '(face spaces tabs))
+(global-whitespace-mode 1)
