f3172479bbc777033f4636a33f172d02ff82cd34
yassh <yassh32@outlook.jp>
2017-04-21 21:31:11 +0900

    update fish_prompt.fish

--- dotfiles/fish/functions/fish_prompt.fish
+++ dotfiles/fish/functions/fish_prompt.fish
@@ -1,5 +1,11 @@
 function fish_prompt
   echo
-  echo (set_color yellow)(pwd)(set_color normal)
-  echo '> '
+
+  if [ (git rev-parse --is-inside-work-tree ^ /dev/null) ]
+    echo (set_color yellow)(pwd)(set_color normal) (git rev-parse --abbrev-ref HEAD)
+  else
+    echo (set_color yellow)(pwd)(set_color normal)
+  end
+
+  echo (date +'%Y-%m-%d %H:%M') ': '
 end
