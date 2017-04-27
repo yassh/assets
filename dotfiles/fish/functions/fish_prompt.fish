c12a8ad360521dcef8ddf5eaf289da52462f4289
yassh <yassh32@outlook.jp>
2017-04-27 23:02:13 +0900

    update fish prompt

--- dotfiles/fish/functions/fish_prompt.fish
+++ dotfiles/fish/functions/fish_prompt.fish
@@ -2,17 +2,10 @@ function fish_prompt
   echo
 
   if [ (git rev-parse --is-inside-work-tree ^ /dev/null) ]
-    echo\
-      (set_color yellow)(pwd)(set_color normal)\
-      (git rev-parse --abbrev-ref HEAD)\
-      "|"\
-      (git rev-list --count @\{u\}..HEAD ^ /dev/null)"-"(git rev-list --count HEAD..@\{u\} ^ /dev/null)\
-      "|"\
-      (git config user.name) "<"(git config user.email)">"\
-    ;
+    echo (set_color yellow)(pwd)(set_color normal) (git rev-parse --abbrev-ref HEAD) '|' (git config user.name) '<'(git config user.email)'>'
   else
     echo (set_color yellow)(pwd)(set_color normal)
   end
 
-  echo (date +'%Y-%m-%d %H:%M')"> "
+  echo (date +'%Y-%m-%d %H:%M')'> '
 end
--- dotfiles/fish/functions/fish_right_prompt.fish
+++ dotfiles/fish/functions/fish_right_prompt.fish
@@ -1,2 +1,3 @@
 function fish_right_prompt
+  echo 🐟
 end
