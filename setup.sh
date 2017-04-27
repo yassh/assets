0aec5caff7f01edbe0f34f6e633a5a0cf37f15c1
yassh <yassh32@outlook.jp>
2017-04-28 03:22:09 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# http://ja.stackoverflow.com/questions/17274
-__DIR__=$(cd $(dirname $0) && pwd)
+__DIR__=$(dirname "$(which "$0")")
 
-$__DIR__/dotfiles/setup.sh
+"$__DIR__/dotfiles/setup.sh"
 
-if [ ! -e ~/.anyenv ]; then
+if test ! -e ~/.anyenv; then
   git clone https://github.com/riywo/anyenv ~/.anyenv
 fi
 
-$__DIR__/shellscripts/brew-install.sh
-$__DIR__/shellscripts/go-get.sh
-$__DIR__/shellscripts/npm-install.sh
-$__DIR__/shellscripts/pip-install.sh
+"$__DIR__/shellscripts/brew-install.sh"
+"$__DIR__/shellscripts/go-get.sh"
+"$__DIR__/shellscripts/npm-install.sh"
+"$__DIR__/shellscripts/pip-install.sh"
 
-cd $__DIR__/nodescripts && npm install
+cd "$__DIR__/nodescripts" && npm install
