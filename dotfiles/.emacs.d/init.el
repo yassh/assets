e1fbb019230bdb2461f7ab7593f34894709476bd
yassh <yassh32@outlook.jp>
2017-05-09 15:01:35 +0900

    add emacs packages

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -16,3 +16,23 @@
 
 ;; 対応する括弧を強調表示する
 (show-paren-mode t)
+
+;; MELPA
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (neotree zenburn-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load-theme 'zenburn t)
+(global-set-key [f8] 'neotree-toggle)
