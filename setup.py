5eb001ea9820bbc1f3b781c6098d00aeece5d907
yassh <yassh32@outlook.jp>
2017-09-02 01:28:31 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
 
-from os import system
 from os import path
+from os import system
 from shutil import which
+from sys import platform
 
 __dir__ = path.dirname(path.realpath(__file__))
 __home__ = path.expanduser('~')
 
 # setup dotfiles
 system(path.join(__dir__, 'dotfiles', 'setup.sh'))
-
-# setup fzf
-if not path.exists(path.join(__home__, '.fzf')):
-    system('git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf')
-    system('~/.fzf/install')
 
 # install anyenv
 if not path.exists(path.join(__home__, '.anyenv')):
@@ -24,8 +20,20 @@ if not which('ndenv'):
     system('anyenv install ndenv')
     system('ndenv install v8 && ndenv global v8 && ndenv rehash')
 
+# install nodescripts
+system('cd {} && npm install'.format(path.join(__dir__, 'nodescripts')))
+
 # install packages
 system('package-install.py all')
 
-# install nodescripts
-system('cd {} && npm install'.format(path.join(__dir__, 'nodescripts')))
+# install fzf
+if not path.exists(path.join(__home__, '.fzf')):
+    system('git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf')
+    system('~/.fzf/install')
+
+# install micro
+if not which('micro'):
+    if platform.startswith('linux'):
+        if not path.exists(path.join(__home__, 'bin')):
+            system('mkdir ~/bin')
+        system('curl -sL https://gist.githubusercontent.com/zyedidia/d4acfcc6acf2d0d75e79004fa5feaf24/raw/a43e603e62205e1074775d756ef98c3fc77f6f8d/install_micro.sh | bash -s linux64 ~/bin')
