84032eeff695262fca6db9b5187433a07c0d10da
yassh <yassh32@outlook.jp>
2018-01-23 12:50:42 +0900

    remove git-config-user-recursive.sh

--- shellscripts/git-config-user-recursive.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git config user.name "yassh" && git config user.email "yassh32@outlook.jp"
-git submodule foreach --recursive 'git config user.name "yassh" && git config user.email "yassh32@outlook.jp"'
