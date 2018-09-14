11a0a772c662f72216525aad72c3636a85b6ed57
yassh <yassh32@outlook.jp>
2018-09-14 16:31:44 +0900

    update .hyper.js

--- dotfiles/.hyper.js
+++ dotfiles/.hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: "stable",
 
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 13,
 
     // font family with optional fallbacks
     fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
@@ -133,7 +133,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ["hyper-one-light"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
