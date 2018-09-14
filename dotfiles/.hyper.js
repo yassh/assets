53751abd0539ad9548e2c01b638b65666ce90f3d
yassh <yassh32@outlook.jp>
2018-09-14 16:24:04 +0900

    update .hyper.js

--- dotfiles/.hyper.js
+++ dotfiles/.hyper.js
@@ -104,7 +104,9 @@ module.exports = {
     shellArgs: ['--login'],
 
     // for environment variables
-    env: {},
+    env: {
+      LANG: 'ja_JP.UTF-8'
+    },
 
     // set to `false` for no bell
     bell: 'SOUND',
