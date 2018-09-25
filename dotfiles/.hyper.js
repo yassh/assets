7a81ceb6f966608d9a2c8d72ad5d7ab9acbd000f
yassh <yassh32@outlook.jp>
2018-09-25 11:00:07 +0900

    install hyper-ink-badge

--- dotfiles/.hyper.js
+++ dotfiles/.hyper.js
@@ -133,7 +133,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-one-light"],
+  plugins: ["hyper-one-light", "hyper-ink-badge"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
